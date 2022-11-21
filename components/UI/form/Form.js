1f5688d6d2e080a0a3176dce406ec1bbb9b34762
sammy <dappuprashanth43@gmail.com>
2022-11-21 10:36:41 +0530

    reverted again

--- components/UI/form/Form.js
+++ components/UI/form/Form.js
@@ -32,37 +32,12 @@ function Form() {
         body: formData,
     })
     .then((res) => res.json())
-    .then((response) => {
-
-      if(res.status >= 200 && res.status <=205) {
-        console.log('Success:', data);
-        const {fund} = data
-        router.push("/fundraisers/" + fund._id)
-      } else if(res.status >= 400 && res.status <= 405)
-      {
-        if((response.toString()).includes("Invalid User") || (response.toString()).includes("User not logged in ") || (response.toString()).includes("User Not Found") || (response.toString()).includes("Please Login to Access"))
-        {
-          //Route to LOGIN and change global variable status!!
-        }
-        else
-        {
-          if((response.error).toString() != "Something went wrong")
-          {
-            alert((response.message).toString());
-          }
-          else{
-            alert((response.error).toString());
-          }
-        }
-        
-      }
-      else
-      {
-        alert(response.message);
-      }
-      
+    .then((data) => {
+      console.log('Success:', data);
+      const {fund} = data
+      router.push("/fundraisers/" + fund._id)
     })
-    
+    alert(JSON.stringify(`${res.message}, status: ${res.status}`));
 
     
 
