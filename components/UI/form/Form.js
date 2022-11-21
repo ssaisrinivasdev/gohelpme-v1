4a127c825bc2d98cd16a357618953570018ebfcc
PrashanthSammy <108586208+PrashanthSammy@users.noreply.github.com>
2022-11-21 13:18:48 +0530

    removed uploading feature

--- components/UI/form/Form.js
+++ components/UI/form/Form.js
@@ -14,40 +14,41 @@ function Form() {
 
   const onSubmit = async data => {
 
-    //const jsonData = JSON.stringify(data)
+    const jsonData = JSON.stringify(data)
     console.log(data)
 
     // const formData = new FormData();
     // formData.append("images", data.images[0]);
-  const formData  = new FormData();
+//   const formData  = new FormData();
 	  
-	for(const name in data) {
-	    formData.append(name, data[name]);
-	}
-	for(var i =0;i < data.images.length;i++){
-		formData.append("images",data.images[i]);
-	}
+// 	for(const name in data) {
+// 	    formData.append(name, data[name]);
+// 	}
+	  
+// 	for(let i =0;i < data.images.length;i++){
+// 		return formData.append("images",data.images[i]);
+// 	}
   
 
 	  
   // formData.append("images", data.images[0]);
 	  
-	  console.log(formData)
+// 	  console.log(formData)
     const res = await fetch("http://gohelpme.online/api/createfund", {
         method: "POST",
 	headers: {
 	      'Content-Type': 'multipart/form-data'
 	      // 'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: formData,
+        body: jsonData,
     })
     .then((res) => res.json())
-    .then((data) => {
-      console.log('Success:', data);
-      const {fund} = data
+    .then((response) => {
+      console.log('Success:', response);
+      const {fund} = response
       router.push("/fundraisers/" + fund._id)
     })
-    alert(JSON.stringify(`${res.message}, status: ${res.status}`));
+//     alert(JSON.stringify(`${res.message}, status: ${res.status}`));
 
 
 
@@ -209,29 +210,29 @@ function Form() {
 
           {/* Images  */}
 
-              <label
-              for="dropzone-file"
-              className="flex items-center px-3 py-3 mx-auto mt-6 text-center bg-white border-2 border-dashed rounded-md cursor-pointer "
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 text-gray-300 "
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                />
-              </svg>
+//               <label
+//               for="dropzone-file"
+//               className="flex items-center px-3 py-3 mx-auto mt-6 text-center bg-white border-2 border-dashed rounded-md cursor-pointer "
+//             >
+//               <svg
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 className="w-6 h-6 text-gray-300 "
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 stroke="currentColor"
+//                 stroke-width="2"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+//                 />
+//               </svg>
 
-              <h2 className="mx-3 text-gray-400">Upload Images</h2>
+//               <h2 className="mx-3 text-gray-400">Upload Images</h2>
 
-              <input type="file" multiple {...register("images")} />
-            </label>
+//               <input type="file" multiple />
+//             </label>
 
 
 
