5f0105d18fbc2a3241a5b33d5138303ef3e30c26
sammy <dappuprashanth43@gmail.com>
2022-11-27 18:47:51 +0530

    removed react import

--- components/UI/CategoryFunds.js
+++ components/UI/CategoryFunds.js
@@ -1,71 +1,53 @@
-import React from "react";
 import DefaultTitle from "./DefaultTitle";
 import Cards from "./FundCards";
 import PageTitle from "./PageTitle";
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 
 function CategoryFunds({ title, desc, categoryTitle }) {
-
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-  
-  handler()
-  
+    handler();
   }, []);
-  
-  async function handler () {
-  
-  let result = await fetch("http://gohelpme.online/api/user/637a3e91d5edc93ca1fc446e", {
-  
-  method: "GET",
-  
-  headers: {
-  
-  "Content-Type": "application/json",
-  
-  "Accept": "application/json"
-  
-  }
-  
-  })
-  
-  if(result.status>=200 && result.status<=300){
-  
-  const jsonresultData = await result.json();
-  
-  //console.log(jsonresultData.response.created_funds)
-  
-  let createdFunds
-  
-  if(jsonresultData){
-  
-  createdFunds = jsonresultData.response.created_funds.map((items) => {
-  
-  return (
-  
-  <Cards items = {items}/>
-  
-  )
-  
-  })}
-  
-  setData(createdFunds)
-  
-  }
-  
-  }
 
+  async function handler() {
+    let result = await fetch(
+      "http://gohelpme.online/api/user/637a3e91d5edc93ca1fc446e",
+      {
+        method: "GET",
 
+        headers: {
+          "Content-Type": "application/json",
+
+          Accept: "application/json",
+        },
+      }
+    );
+
+    if (result.status >= 200 && result.status <= 300) {
+      const jsonresultData = await result.json();
+
+      //console.log(jsonresultData.response.created_funds)
+
+      let createdFunds;
+
+      if (jsonresultData) {
+        createdFunds = jsonresultData.response.created_funds.map((items) => {
+          return <Cards items={items} />;
+        });
+      }
+
+      setData(createdFunds);
+    }
+  }
 
   return (
     <div>
       <PageTitle title={title} desc={desc} />
       <DefaultTitle title={categoryTitle} />
       <div className="grid grid-flow-row col-auto grid-cols-1 items-center text-center mx-auto">
-      <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
-
-{data}
+        <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
+          {data}
         </div>
       </div>
     </div>
