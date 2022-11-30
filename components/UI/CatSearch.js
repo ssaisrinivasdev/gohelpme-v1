42fa4bfe0a734ca4db32ef0db201b407eb5c0044
sammy <dappuprashanth43@gmail.com>
2022-12-01 03:28:25 +0530

    md optimized cat search

--- components/UI/CatSearch.js
+++ components/UI/CatSearch.js
@@ -22,60 +22,62 @@ function CatSearch({ placeholder }) {
       onSubmit={handleSubmit(searchNow)}
       className="flex flex-col gap-4 xl:flex-row xl:w-[980px] xl:mx-auto py-5"
     >
-      <div className="xl:w-[150px] w-64 mx-auto">
-        <select
-          {...register("category")}
-          className=" w-56 py-3 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white"
-        >
-          <option value="Medical">Medical</option>
-          <option value="Memorial">Memorial</option>
-          <option value="Emergency">Emergency</option>
-          <option value="NonProfit">NonProfit</option>
-          <option value="Emergency">FinancialEmergency</option>
-          <option value="Animals">Animals</option>
-          <option value="Environment">Environment</option>
-          <option value="Business">Business</option>
-          <option value="Community">Community</option>
-          <option value="Competition">Competition</option>
-          <option value="Creative">Creative</option>
-          <option value="Event">Event</option>
-          <option value="Faith">Faith</option>
-          <option value="Family">Family</option>
-          <option value="Sports">Sports</option>
-          <option value="Wishes">Wishes</option>
-          <option value="Travel">Travel</option>
-          <option value="Volunteer">Volunteer</option>
-          <option value="Others">Others</option>
-        </select>
-      </div>
-      <div className="w-[450px]">
-        <div className="relative">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+      <div className="flex mx-3 gap-2">
+        <div className="xl:w-[150px] xl:mx-auto">
+          <select
+            {...register("category")}
+            className=" w-36 py-3 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            <option value="Medical">Medical</option>
+            <option value="Memorial">Memorial</option>
+            <option value="Emergency">Emergency</option>
+            <option value="NonProfit">NonProfit</option>
+            <option value="Emergency">FinancialEmergency</option>
+            <option value="Animals">Animals</option>
+            <option value="Environment">Environment</option>
+            <option value="Business">Business</option>
+            <option value="Community">Community</option>
+            <option value="Competition">Competition</option>
+            <option value="Creative">Creative</option>
+            <option value="Event">Event</option>
+            <option value="Faith">Faith</option>
+            <option value="Family">Family</option>
+            <option value="Sports">Sports</option>
+            <option value="Wishes">Wishes</option>
+            <option value="Travel">Travel</option>
+            <option value="Volunteer">Volunteer</option>
+            <option value="Others">Others</option>
+          </select>
+        </div>
+        <div className="w-[300px] xl:w-[450px]">
+          <div className="relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              {...register("search")}
+              type="text"
+              placeholder="Search by fund"
+              className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white"
             />
-          </svg>
-          <input
-            {...register("search")}
-            type="text"
-            placeholder="Search by fund"
-            className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white"
-          />
+          </div>
         </div>
       </div>
       <input
         type="submit"
         value="Search"
-        class="w-[200px] px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-color1 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+        class="max-md:mx-3 xl:w-[200px] px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-color1 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
       />
     </form>
   );
