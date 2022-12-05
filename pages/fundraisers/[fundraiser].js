3ecaa1b11d990b78a023a3c9334e4eab44b3c098
sammy <dappuprashanth43@gmail.com>
2022-12-05 19:21:42 +0530

    commented get static props

--- pages/fundraisers/[fundraiser].js
+++ pages/fundraisers/[fundraiser].js
@@ -1,57 +1,96 @@
+import { useRouter } from "next/router";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import Footer from "../../components/UI/Footer";
 import FundData from "../../components/UI/FundData";
 import Header from "../../components/UI/Header";
 
-function Fundraiser({ fund, following_status }) {
-  console.log(fund);
+function Fundraiser() {
+  const router = useRouter();
+  const { fundraiser } = router.query;
+  console.log(fundraiser)
+
+  const [fundData, setFundData] = useState(null)
+  console.log(fundData)
+
+  useEffect(() => {
+
+    
+
+
+    const sync = async () => {
+      let result = await fetch(
+        "http://gohelpme.online/api/fund/" + fundraiser,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        },
+      
+      );
+
+      const data = await result.json()
+      setFundData(data)
+      
+    };
+
+    sync();
+    
+  },[]);
 
   return (
+
     <div>
       <Header />
-      <FundData fund={fund} followingStatus={following_status} />
+
+      {fundData && <FundData fund={fundData.fund} followingStatus={fundData.following_status} />}
+            
+            
       <Footer />
     </div>
   );
 }
 
-export async function getStaticProps(context) {
-  const { params } = context;
+// export async function getStaticProps(context) {
+//   const { params } = context;
 
-  const fundraiserId = params.fundraiser;
+//   const fundraiserId = params.fundraiser;
 
-  let result = await fetch("http://gohelpme.online/api/fund/" + fundraiserId);
+//   let result = await fetch("http://gohelpme.online/api/fund/" + fundraiserId);
 
-  const data = await result.json();
+//   const data = await result.json();
 
-  const fund = data.fund;
+//   const fund = data.fund;
 
-  const following_status = data.following_status
+//   const following_status = data.following_status
 
-  // let fund = posts.find(fund => fund._id === fundraiserId)
+//   // let fund = posts.find(fund => fund._id === fundraiserId)
 
-  return {
-    props: {
-      fund,
-      following_status
-    },
-    revalidate: 5,
-  };
-}
+//   return {
+//     props: {
+//       fund,
+//       following_status
+//     },
+//     revalidate: 5,
+//   };
+// }
 
-export async function getStaticPaths() {
-  let result = await fetch("http://gohelpme.online/api/funds");
+// export async function getStaticPaths() {
+//   let result = await fetch("http://gohelpme.online/api/funds");
   
-  const data = await result.json();
+//   const data = await result.json();
 
-  const ids = data.funds.map((fund) => fund._id);
+//   const ids = data.funds.map((fund) => fund._id);
 
-  const pathsWithParams = ids.map((id) => ({ params: { fundraiser: id } }));
+//   const pathsWithParams = ids.map((id) => ({ params: { fundraiser: id } }));
 
-  return {
-    paths: pathsWithParams,
-    fallback: "blocking", // can also be true or 'blocking'
-  };
-}
+//   return {
+//     paths: pathsWithParams,
+//     fallback: "blocking", // can also be true or 'blocking'
+//   };
+// }
 
 export default Fundraiser;
