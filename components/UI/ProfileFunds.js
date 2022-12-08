eeccf2d82315bf8976757fd12c47222594ff2aa7
sammy <dappuprashanth43@gmail.com>
2022-12-08 09:42:15 +0530

    profile page edited

--- components/UI/ProfileFunds.js
+++ components/UI/ProfileFunds.js
@@ -3,13 +3,17 @@ import Cards from "./FundCards";
 import PageTitle from "./PageTitle";
 import { React, useEffect, useState } from "react";
 import jwt from "jsonwebtoken";
-import Skeleton from '@mui/material/Skeleton';
+import Skeleton from "@mui/material/Skeleton";
 
 function ProfileFunds({ title, desc, categoryTitle }) {
-  const [created_funds, setCreated_FundsData] = useState(null)
-  const [following_funds, setFollowing_FundsData] = useState(null)
-  const [donated_funds, setDonated_FundsData] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
+  const [created_funds, setCreated_FundsData] = useState(null);
+  const [following_funds, setFollowing_FundsData] = useState(null);
+  const [donated_funds, setDonated_FundsData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  console.log(created_funds);
+  console.log(following_funds);
+  console.log(donated_funds);
 
   useEffect(() => {
     handler();
@@ -41,29 +45,32 @@ function ProfileFunds({ title, desc, categoryTitle }) {
 
       //console.log(jsonresultData.response.created_funds)
 
-      let createdFunds, donateFunds, followingFunds      
+      let createdFunds, donateFunds, followingFunds;
       if (jsonresultData) {
-        createdFunds = jsonresultData.response.created_funds == '[]' ? false : (jsonresultData.response.created_funds.map((items) => {
-          return (
-            <Cards key={items._id} items={items} />
-          )
-        }))
-        donateFunds = jsonresultData.response.donated_funds == '[]' ? false : (jsonresultData.response.donated_funds.map((items) => {
-          return (
-            <Cards key={items._id} items={items} />
-          )
-        }))
-        followingFunds = jsonresultData.response.followed_funds == '[]' ? false : (jsonresultData.response.followed_funds.map((items) => {
-          return (
-            <Cards key={items._id} items={items} />
-          )
-        }))
+        createdFunds =
+          jsonresultData.response.created_funds == "[]"
+            ? false
+            : jsonresultData.response.created_funds.map((items) => {
+                return <Cards key={items._id} items={items} />;
+              });
+        donateFunds =
+          jsonresultData.response.donated_funds == "[]"
+            ? false
+            : jsonresultData.response.donated_funds.map((items) => {
+                return <Cards key={items._id} items={items} />;
+              });
+        followingFunds =
+          jsonresultData.response.followed_funds == "[]"
+            ? false
+            : jsonresultData.response.followed_funds.map((items) => {
+                return <Cards key={items._id} items={items} />;
+              });
       }
-      setCreated_FundsData(createdFunds)
-      setFollowing_FundsData(followingFunds)
-      setDonated_FundsData(donateFunds)
-      setIsLoading(false)
-      console.log(donated_funds)
+      setCreated_FundsData(createdFunds);
+      setFollowing_FundsData(followingFunds);
+      setDonated_FundsData(donateFunds);
+      setIsLoading(false);
+      console.log(donated_funds);
     }
   }
 
@@ -81,80 +88,87 @@ function ProfileFunds({ title, desc, categoryTitle }) {
   return (
     <div className="mx-auto">
       <div>
-        <DefaultTitle title="Your Funds"/>
-        {isLoading ? (
+        <DefaultTitle title="Your Funds" />
+        {isLoading && created_funds != null ? (
           <div>
             <Skeleton width="20%" />
             <Skeleton
               animation="wave"
               variant="rectangular"
-              width="80%" 
+              width="80%"
               height={380}
               style={{ marginBottom: 16, marginTop: 16 }}
             />
           </div>
-          )
-        :(
-        <div className="grid grid-flow-row col-auto grid-cols-1 items-center mx-auto">
-          <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
-            
-            {(created_funds != null) ? created_funds : (<p className="text-left mx-10 mb-10">You haven't created any funds yet</p>)}
-
-          </div>
-        </div>
-        )
-        }
-      </div>
-      <div>
-        <DefaultTitle title="Donated Funds" />
-        {isLoading ? (
-          <div>
-          
-            <Skeleton width="20%" />
-            <Skeleton
-              animation="wave"
-              variant="rectangular"
-              width="80%" 
-              height={380}
-              style={{ marginBottom: 16, marginTop: 16 }}
-            />
-          </div>
-        ):(
-          <div className="grid grid-flow-row col-auto grid-cols-1 items-center text-center mx-auto">
+        ) : (
+          <div className="grid grid-flow-row col-auto grid-cols-1 items-center mx-auto">
             <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
-            
-              {(donated_funds != null) ? donated_funds : (<p className="text-left mx-10 mb-10">You haven't donated yet</p>)}
-            </div>
-          </div>
-        )
-        }
-      </div>
-      <div>
-        <DefaultTitle title="Following Funds" />
-        {isLoading ? (
-          <div>
-            <Skeleton width="20%" />
-            <Skeleton
-              animation="wave"
-              variant="rectangular"
-              width="80%" 
-              height={380}
-              style={{ marginBottom: 16, marginTop: 16 }}
-            />
-          </div>
-        ):(
-          <div className="grid grid-flow-row col-auto grid-cols-1 items-center text-center mx-auto">
-            <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
-              {(following_funds != null) ? following_funds : (<p className="text-left mx-10 mb-10">You haven't followed any funds</p>)}
-
+              {created_funds != null ? (
+                created_funds
+              ) : (
+                <p className="text-left mb-10">
+                  You haven't created any funds yet
+                </p>
+              )}
             </div>
           </div>
         )}
       </div>
-    </div>
-  );
-
-
+      <div>
+        <DefaultTitle title="Donated Funds" />
+        {isLoading && donated_funds != null ? (
+          <div>
+            <Skeleton width="20%" />
+            <Skeleton
+              animation="wave"
+              variant="rectangular"
+              width="80%"
+              height={380}
+              style={{ marginBottom: 16, marginTop: 16 }}
+            />
+          </div>
+        ) : (
+          <div className="grid grid-flow-row col-auto grid-cols-1 items-center text-center mx-auto">
+            <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
+              {donated_funds != null ? (
+                donated_funds
+              ) : (
+                <p className="text-left mb-10">You haven't donated yet</p>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <DefaultTitle title="Following Funds" />
+        {isLoading && following_funds != null ? (
+          <div>
+            <Skeleton width="20%" />
+            <Skeleton
+              animation="wave"
+              variant="rectangular"
+              width="80%"
+              height={380}
+              style={{ marginBottom: 16, marginTop: 16 }}
+            />
+          </div>
+        ) : (
+          <div className="grid grid-flow-row col-auto grid-cols-1 items-center text-center mx-auto">
+            <div className="w-full overflow-auto whitespace-nowrap scroll-smooth">
+              {following_funds != null ? (
+                following_funds
+              ) : (
+                <p className="text-left mb-10">
+                  You haven't followed any funds
+                </p>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+          
+    </div>
+  );
 }
 
 export default ProfileFunds;
