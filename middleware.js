0fd2038f364e2ef389a12173a9b1287726079368
sammy <dappuprashanth43@gmail.com>
2022-12-09 17:56:05 +0530

    testing with out includes

--- middleware.js
+++ middleware.js
@@ -4,11 +4,11 @@ export default function middleware(req) {
   let verify = req.cookies.get("token");
   let url = req.url;
 
-  if (verify && url.includes("http://gohelpme.online/login")) {
+  if (verify && url === "http://gohelpme.online/login") {
     return NextResponse.redirect("http://gohelpme.online/dashboard");
   }
 
-  if (!verify && url.includes("http://gohelpme.online/dashboard")) {
+  if (!verify && url === "http://gohelpme.online/dashboard")) {
     return NextResponse.redirect("http://gohelpme.online/login");
   }
 }
