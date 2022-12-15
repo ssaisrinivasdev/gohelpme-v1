c8c944e1fcfc446141fc7378e9ca8daba73409a8
sammy <dappuprashanth43@gmail.com>
2022-12-15 11:58:50 +0530

    profile page edits

--- components/UI/ProfileSidebar.js
+++ components/UI/ProfileSidebar.js
@@ -8,48 +8,10 @@ function Sidebar({ segmentActive }) {
   };
 
   return (
-    <div className="flex h-screen w-16 flex-col justify-between border-r bg-white sticky top-0">
+    <div className=" mx-5 flex h-48 w-16 flex-col justify-between border-r bg-white sticky top-0">
       <div>
-        <div className="inline-flex h-16 w-16 items-center justify-center">
-          <span className="block h-10 w-10 rounded-lg bg-gray-200"></span>
-        </div>
-
         <div className="border-t border-gray-100">
           <nav aria-label="Main Nav" className="flex flex-col p-2">
-            <div className="py-4">
-              <Link
-                onClick={() => {
-                  handler({ param: "dashboard" });
-                }}
-                href=""
-                className="t group relative flex justify-center rounded bg-blue-50 px-2 py-1.5 text-blue-700"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-10 w-10 opacity-75"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-
-                <span className="absolute left-full top-1/2 ml-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100">
-                  Dashboard
-                </span>
-              </Link>
-            </div>
-
             <ul className="space-y-1 border-t border-gray-100 pt-4">
               <li
                 onClick={() => {
@@ -77,6 +39,36 @@ function Sidebar({ segmentActive }) {
 
                   <span className="absolute left-full top-1/2 ml-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100">
                     Financial Stats
+                  </span>
+                </Link>
+              </li>
+
+              <li
+                onClick={() => {
+                  handler({ param: "fundApproval" });
+                }}
+              >
+                <Link
+                  href=""
+                  className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-10 h-10 opacity-75"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+                    />
+                  </svg>
+
+                  <span className="absolute left-full top-1/2 ml-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white opacity-0 group-hover:opacity-100">
+                    Fund Approval
                   </span>
                 </Link>
               </li>
