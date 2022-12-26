e1fbbcdc735c422102a4cc1b05937f7003122c24
sammy <dappuprashanth43@gmail.com>
2022-12-26 21:50:31 +0530

    userfetch trails

--- hooks/use-userFetch.js
+++ hooks/use-userFetch.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import jwt from "jsonwebtoken";
 
@@ -8,22 +8,22 @@ function useUserFetch() {
   }, []);
 
   async function handler() {
+    const [id, setId] = useState(null);
     const token = localStorage.getItem("token");
-    let id = null;
+    // let id = null;
 
     if (token) {
       jwt.verify(token, "$tr0ngkEy123!@#", function (err, decoded) {
         if (err) {
           console.log("no err");
         } else {
-          id = decoded.id;
+          setId(decoded.id);
         }
       });
     }
 
     let result = await fetch("http://gohelpme.online/api/user/" + id, {
       method: "GET",
-
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
