70f38650e6ef47e9da06779f9894420d27882f0b
sammy <dappuprashanth43@gmail.com>
2022-12-26 22:43:01 +0530

    trails

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -16,7 +16,7 @@ function Dashboard() {
   const [profileButtonActive, setProfileButtonActive] = useState("details");
 
   const userIn = useUserFetch();
-  console.log(userIn.response);
+  console.log(userIn?.response);
 
   const handler = (data) => {
     setProfileButtonActive(data);
