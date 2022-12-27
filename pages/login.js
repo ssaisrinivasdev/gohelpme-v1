13f44ae57b088145ad2de995ffd08e7fa657f8b9
sammy <dappuprashanth43@gmail.com>
2022-12-27 17:35:03 +0530

    trails

--- pages/login.js
+++ pages/login.js
@@ -10,11 +10,15 @@ import { addDetails } from "../store/actions/user";
 import useLoginCheck from "../hooks/use-logincheck";
 import Cookies from "universal-cookie";
 import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Login() {
   const isLoggedIn = useLoginCheck();
   const dispatch = useDispatch();
   const cookies = new Cookies();
+
+  const { data: session } = useSession();
+  console.log(session);
 
   const {
     register,
