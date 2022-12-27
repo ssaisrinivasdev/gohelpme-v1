90568f8482d7718f3ad926dffe283db90428d28e
Sai Srinivas <ssaisrinivas616@gmail.com>
2022-12-27 23:44:45 +0530

    reverted back admin auth

--- pages/adminpanel/index.js
+++ pages/adminpanel/index.js
@@ -15,71 +15,71 @@ import Box from '@mui/material/Box';
 
 function Admin() {
   const [buttonActive, setButtonActive] = useState("dashboard");
-  const [isLoading,setIsLoading] = useState(true);
+  const [isLoading,setIsLoading] = useState(false);
   const [roles, setRoles] = useState("");
-  const [adminType, setAdminType] = useState("");
-  const [email, setEmail] = useState("");
+  const [adminType, setAdminType] = useState("admin");
+  const [email, setEmail] = useState("admin@admin.com");
   const router = useRouter();
   const [canRoute, setCanRoute] = useState(true);
 
 
-  useEffect(() => {
-    getAdminDetails();
-  }, [buttonActive]);
+  // useEffect(() => {
+  //   getAdminDetails();
+  // }, [buttonActive]);
 
-  async function getAdminDetails() {
-    const token = localStorage.getItem("admin_token");
-    let id = null;
-    if (token) {
-      jwt.verify(token, "$tr0ngkEy123!@#", function (err, decoded) {
-        if (!err) {
-          id = decoded.id;
-        }else{
-          console.error("The below one is the error")
-          console.error(err);          
-          setCanRoute(false);
-        }
-      });
-    }else{
-      console.error("Token not found");
-      setCanRoute(false);
-    }
-    if(id != null){
-      let result = await fetch("http://gohelpme.online/api/admin/details/" + id, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, authorization",
-          "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE"
-        },
-      });
-      const response = await result.json();
-      if(result.status >= 200 && result.status < 300){
-        console.log(response)
-        setAdminType(response.response.admin_type);
-        setRoles(response.response.roles);
-        setEmail(response.response.email);
-        setIsLoading(false);
-      }else if(result.status == 401){
-        if (
-          response.toString().includes("Invalid User") ||
-          response.toString().includes("User not logged in ") ||
-          response.toString().includes("User Not Found") ||
-          response.toString().includes("Please Login to Access")
-        ) {
-          setCanRoute(false);
-        }
-      }
-    }else{
-      setCanRoute(false);
-    }
-    if(!canRoute){
-      router.push("/adminpanel/login");
-    }
+  // async function getAdminDetails() {
+  //   const token = localStorage.getItem("admin_token");
+  //   let id = null;
+  //   if (token) {
+  //     jwt.verify(token, "$tr0ngkEy123!@#", function (err, decoded) {
+  //       if (!err) {
+  //         id = decoded.id;
+  //       }else{
+  //         console.error("The below one is the error")
+  //         console.error(err);          
+  //         setCanRoute(false);
+  //       }
+  //     });
+  //   }else{
+  //     console.error("Token not found");
+  //     setCanRoute(false);
+  //   }
+  //   if(id != null){
+  //     let result = await fetch("http://gohelpme.online/api/admin/details/" + id, {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Accept": "application/json",
+  //         "Access-Control-Allow-Origin": "*",
+  //         "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, authorization",
+  //         "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, DELETE"
+  //       },
+  //     });
+  //     const response = await result.json();
+  //     if(result.status >= 200 && result.status < 300){
+  //       console.log(response)
+  //       setAdminType(response.response.admin_type);
+  //       setRoles(response.response.roles);
+  //       setEmail(response.response.email);
+  //       setIsLoading(false);
+  //     }else if(result.status == 401){
+  //       if (
+  //         response.toString().includes("Invalid User") ||
+  //         response.toString().includes("User not logged in ") ||
+  //         response.toString().includes("User Not Found") ||
+  //         response.toString().includes("Please Login to Access")
+  //       ) {
+  //         setCanRoute(false);
+  //       }
+  //     }
+  //   }else{
+  //     setCanRoute(false);
+  //   }
+  //   if(!canRoute){
+  //     router.push("/adminpanel/login");
+  //   }
 
-  }
+  // }
 
   const handler = (data) => {
     console.log(data);
