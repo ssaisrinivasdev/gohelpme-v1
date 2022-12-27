2c70cfefcd5b29a6bbd8bb1133322257e5d8bac6
sammy <dappuprashanth43@gmail.com>
2022-12-27 17:29:49 +0530

    nextauth trails

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -46,7 +46,7 @@ export default NextAuth({
         const user = await res.json();
 
         // If no error and we have user data, return it
-        if (res.ok && username == user.response.email) {
+        if (res.ok && user) {
           return user;
         }
         // Return null if user data could not be retrieved
